d683f9f0ca45e172add6900bc4f33b5932e8343f
robi <kali_robi@hotmail.com>
2024-10-26 19:11:34 +0300

    changing to ubuntu due to compatibility issues with python 3.10

--- mezoapp/ec2userdata.sh
+++ mezoapp/ec2userdata.sh
@@ -1,12 +1,13 @@
-
-
-sudo yum update -y
-sudo yum install python3 python3-pip git -y
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y python3.10 python3-pip git
 sudo mkdir -p /var/www/mezoappproto
 cd /var/www/mezoappproto
-git clone https://github.com/KaliRobi/mezoappprotoype.git
-
-sudo pip3 install virtualenv
-virtualenv venv
+git clone https://github.com/KaliRobi/mezoappsingle.git
+python3.10 -m venv venv
 source venv/bin/activate
-
+pip install -r mezoappsingle/requirements.txt
+cd mezoappsingle/mezoapp/
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
